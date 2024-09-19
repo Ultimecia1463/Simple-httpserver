b5afe1e7ce1ad46952fe29ea985821932b2e1068
Ansh_sharma <keshu1234harshu@gmail.com>
2024-09-19 23:16:31 +0530

    added logger

--- src/main/java/com/ultimecia/httpserver/Httpserver.java
+++ src/main/java/com/ultimecia/httpserver/Httpserver.java
@@ -1,19 +1,27 @@
 package com.ultimecia.httpserver;
 import java.io.IOException;
 
+import org.slf4j.LoggerFactory;
+
 import com.ultimecia.httpserver.config.Configuration;
 import com.ultimecia.httpserver.config.Configurationmanager;
 import com.ultimecia.httpserver.core.ServerListenerThread;
 
+import ch.qos.logback.classic.Logger;
+
 public class Httpserver {
+
+    private final static Logger LOGGER = (Logger) LoggerFactory.getLogger(Httpserver.class);
+
     public static void main(String[] args) {
-        System.out.println("server starting......");
+
+        LOGGER.info("server starting......");
 
         Configurationmanager.getinstance().loadConfigurationfile("src/main/resources/http.json");
         Configuration conf = Configurationmanager.getinstance().getcurrentConfiguration();
 
-        System.out.println("using port: "+conf.getPort());
-        System.out.println("using webroot: "+ conf.getWebroot());
+        LOGGER.info("using port: "+conf.getPort());
+        LOGGER.info("using webroot: "+ conf.getWebroot());
 
         try {
             ServerListenerThread ServerListenerThread = new ServerListenerThread(conf.getPort(),conf.getWebroot());
