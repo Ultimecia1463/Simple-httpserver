3ff08897b39be5a6f1cc527194381bda45a0f810
Ansh_sharma <keshu1234harshu@gmail.com>
2024-10-13 23:33:06 +0530

    more test cases

--- src/main/java/com/ultimecia/http/HttpRequest.java
+++ src/main/java/com/ultimecia/http/HttpRequest.java
@@ -13,9 +13,15 @@ public class HttpRequest extends HttpMessage{
         return method;
     }
 
-    void setMethod(HttpMethod method) {
-        this.method = method;
+    void setMethod(String methodName) throws HttpParsingException {
+        for(HttpMethod method:HttpMethod.values()){
+            if(methodName.equals(method.name())){
+                this.method=method;
+                return;
+            }
+        }
+        throw new HttpParsingException(
+            HttpStatusCodes.SERVER_ERROR_501_NOT_IMPLEMENTED
+        );
     }
-
-    
 }
--- src/main/java/com/ultimecia/http/Httpparser.java
+++ src/main/java/com/ultimecia/http/Httpparser.java
@@ -16,25 +16,65 @@ public class Httpparser {
     private static final int CR = 0x0D; // 13
     private static final int LF = 0x0A; // 10
 
-    public HttpRequest parseHttpRequest(InputStream inputStream){
+    public HttpRequest parseHttpRequest(InputStream inputStream) throws HttpParsingException{
         InputStreamReader reader = new InputStreamReader(inputStream,StandardCharsets.US_ASCII);
 
         HttpRequest request = new HttpRequest();
 
-        parseRequestLine(reader,request);
+        try {
+            parseRequestLine(reader,request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         parseHeaders(reader,request);
         parseBody(reader,request);
 
         return request;
     }
 
-    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException{
+    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException, HttpParsingException{
+        StringBuilder processingDataBuffer = new StringBuilder();
+
+        boolean methodParsed = false;
+        boolean requestTargetParsed = false;
+
         int _byte;
         while ((_byte=reader.read()) >=0 ) {
+
             if (_byte==CR) {
                 _byte=reader.read();
                 if (_byte==LF) {
+                    LOGGER.debug("Request Line VERSION to Process : {}",processingDataBuffer.toString());
+                    if (!methodParsed || !requestTargetParsed) {
+                        throw new HttpParsingException(HttpStatusCodes.CLIENT_ERROR_400_BAD_REQUEST); 
+                    }
                     return;
+                }else{
+                    throw new HttpParsingException(HttpStatusCodes.CLIENT_ERROR_400_BAD_REQUEST);
+                }
+            }
+
+            if (_byte==SP) {
+
+                if (!methodParsed) {
+
+                    LOGGER.debug("Request Line Method to Process : {}",processingDataBuffer.toString());
+                    request.setMethod(processingDataBuffer.toString());
+                    methodParsed=true;
+                }else if(!requestTargetParsed){
+
+                    LOGGER.debug("Request Line Request to Process : {}",processingDataBuffer.toString());
+                    requestTargetParsed=true;
+                }else{
+                    throw new HttpParsingException(HttpStatusCodes.CLIENT_ERROR_400_BAD_REQUEST);
+                }
+                processingDataBuffer.delete(0, processingDataBuffer.length());
+            }else{
+                processingDataBuffer.append((char)_byte);
+                if(!methodParsed){
+                    if (processingDataBuffer.length()>HttpMethod.MAX_LENGTH) {
+                        throw new HttpParsingException(HttpStatusCodes.SERVER_ERROR_501_NOT_IMPLEMENTED);
+                    }
                 }
             }
         }
